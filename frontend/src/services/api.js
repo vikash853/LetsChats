3d8cabb7eeb4f84cf9090e8464004c0a1ad9fd89
Vikash Shukla <vikashshukla5218@gmail.com>
2026-03-18 11:39:30 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -8,7 +8,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api","https://letschats-1.onrender.com",
   headers: { "Content-Type": "application/json" },
 });
 
