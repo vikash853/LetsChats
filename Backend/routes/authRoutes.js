b9de74deab58b5639d88f1c418ee09bd44185e31
Vikash Shukla <vikashshukla5218@gmail.com>
2026-03-19 01:10:17 +0530

    destructure protect from auth middleware

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express    = require("express");
 const router     = express.Router();
-const protect    = require("../middleware/auth");
+
+const { protect } = require("../middleware/auth");
 const {
   register,
   login,
