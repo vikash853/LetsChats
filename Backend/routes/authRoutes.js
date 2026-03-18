f41a763d317bc5a4cab3aee2e970f7909adf7d2f
Vikash Shukla <vikashshukla5218@gmail.com>
2026-03-19 00:43:13 +0530

    Update authRoutes.js

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express  = require("express");
 const router   = express.Router();
-const protect  = require("../middleware/authMiddleware");
+const protect = require("../middleware/auth");
 const {
   register, login, firebaseLogin, getMe, updateProfile,
 } = require("../Controllers/authController");
